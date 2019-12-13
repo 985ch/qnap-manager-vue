ca05a9521953432b9c39f42f74d8da90a19b469b
陈昊 <2972785641@qq.com>
2019-12-13 11:02:21 +0800

    部分规则变更

--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -3,7 +3,7 @@
     <span>资源路径</span>
     <el-select v-model="curPath" placeholder="请选择">
       <el-option
-        v-for="item in resource_path"
+        v-for="item in resPath"
         :key="item"
         :value="item"
       />
@@ -41,7 +41,6 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import { getList } from '@/api/resource'
 import { parseTime } from '@/utils'
 
@@ -59,17 +58,27 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('system', ['resource_path']),
     groupList() {
       const json = {}
-      for (const item of this.list) {
-        if (!json[item.path])json[item.path] = []
+      const list = this.list
+      for (const item of list) {
+        if (!json[item.path]) {
+          json[item.path] = []
+        }
         json[item.path].push(item)
       }
       return json
     },
+    resPath() {
+      if (!this.list) return []
+      const paths = []
+      for (const key in this.groupList) {
+        paths.push(key)
+      }
+      return paths
+    },
     curList() {
-      if (this.list && this.resource_path && this.curPath !== '') {
+      if (this.list && this.curPath !== '') {
         return this.groupList[this.curPath]
       }
       return []
@@ -82,9 +91,8 @@ export default {
     // 拉取数据
     async fetchData() {
       this.listLoading = true
-      await this.$store.dispatch('system/loadConfig')
-      this.curPath = this.resource_path[0] || ''
       this.list = await getList()
+      this.curPath = this.resPath[0] || ''
       this.listLoading = false
     }
   }
